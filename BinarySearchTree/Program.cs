6ccb2f4b633cc4dd34b18ebf3af1b7ecdc96e997
patt26 <kharchepratik123@gmail.com>
2021-05-11 18:02:49 +0530

    Added Addition of nodes

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+            Console.ReadKey();
+        }
+    }
+}
